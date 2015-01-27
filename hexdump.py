59ccb600e19212d76e5864cc88d6d888e6ad4782
Ryan <rrherlih@umass.edu>
2015-01-26 19:38:38 -0500

    updates 1

--- hexdump.py
+++ hexdump.py
@@ -1,10 +1,15 @@
 #! /usr/bin/env python3.4
 
-files = open('npm-debug.log')
-contents = files.read()
+files = open('PinndItPin.png')
+files = unicode(files, errors='.')
+contents = files.read().decode('utf8')
 #" ".join("{:02x}".format(ord(x)) for x in contents)
-for x in contents:
+
+for x in range(len(contents)):
+	if ord(x) not in range(32, 127):
+		print(00)
 	if x%16 != 0:
-		print("{:02x}".format(ord(x)), end=' ')
+		print("{:02x}".format(ord(contents[x])), end=' ')
 	else:
-		print("{:02x}".format(ord(x)))
+		print("{:02x}".format(ord(contents[x])))
+print()
