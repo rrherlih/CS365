2cce5f901bba9dcf8bbf477346aba281b3d8268a
Ryan <rrherlih@umass.edu>
2015-01-28 12:08:26 -0500

    right column aligned

--- hexdump.py
+++ hexdump.py
@@ -2,21 +2,6 @@
 
 files = open('npm-debug.log')
 contents = files.read()
-# files = unicode(files, errors='.')
-# contents = files.read().decode('utf8')
-#" ".join("{:02x}".format(ord(x)) for x in contents)
-
-for x in range(len(contents)):
-	# if ord(x) not in range(32, 127):
-	# 	print(00)
-	if x%16 == 15:
-		print("{:02x}".format(ord(contents[x])))
-	elif x%16 == 0:
-		print("{:08x}".format(x) + ' ' + "{:02x}".format(ord(contents[x])), end=' ')		
-	else:
-		print("{:02x}".format(ord(contents[x])), end=' ')
-print()
-
 
 numChars = 0
 while len(contents) > 0:
@@ -30,10 +15,12 @@ while len(contents) > 0:
 			asciiValues = asciiValues + '.'
 		else:
 			asciiValues = asciiValues + x
+	if len(hexValues) != 48:
+		while len(hexValues) < 48:
+			hexValues = hexValues + '   '
 	print("{:08x}".format(numChars), end='  ')
 	print(hexValues, sep=' ', end='  ')
 	print('|' + asciiValues + '|')
-	print()
 	numChars = numChars + 16
 
 
