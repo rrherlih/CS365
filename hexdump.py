36483c9987baf3320b90326e835401cf261d6825
Ryan <rrherlih@umass.edu>
2015-01-28 19:40:20 -0500

    takes cmd line args

--- hexdump.py
+++ hexdump.py
@@ -1,31 +1,8 @@
 #! /usr/bin/env python3.4
 
-# files = open('npm-debug.log')
-# contents = files.read()
-
-# numChars = 0
-# while len(contents) > 0:
-# 	line = contents[0:15]
-# 	contents = contents[16:len(contents)]
-# 	hexValues = ''
-# 	asciiValues = ''
-# 	for x in line:
-# 		hexValues = hexValues + "{:02x}".format(ord(x)) + ' '
-# 		if ord(x) not in range(32, 127):
-# 			asciiValues = asciiValues + '.'
-# 		else:
-# 			asciiValues = asciiValues + x
-# 	if len(hexValues) != 48:
-# 		while len(hexValues) < 48:
-# 			hexValues = hexValues + '   '
-# 	print("{:08x}".format(numChars), end='  ')
-# 	print(hexValues, sep=' ', end='  ')
-# 	print('|' + asciiValues + '|')
-# 	numChars = numChars + 16
-
 import sys
 
-with open('npm-debug.log', 'rb') as f:
+with open(sys.argv[1], 'rb') as f:
 	numBytes = 0
 	while True:
 		try:
@@ -42,7 +19,7 @@ with open('npm-debug.log', 'rb') as f:
 		except:
 			print(sys.exc_info()[0])
 		print('   '*(16-len(line)), end='')
-		print('|', end='')
+		print(' |', end='')
 		try:
 			for x in line:
 				if x not in range(32, 127):
