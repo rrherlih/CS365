d5e5402a1e6ae64c1cbe50dc2a875a1e0e4dd658
Ryan <rrherlih@umass.edu>
2015-02-18 19:43:05 -0500

    hw3 ifd tags

--- 365-exif.py
+++ 365-exif.py
@@ -9,6 +9,8 @@ from struct import unpack
 
 class UnpackJPEG:
 
+	
+
 	def open_file(self):
 		try:
 			self.fd = open(sys.argv[1], 'rb')
@@ -26,59 +28,118 @@ class UnpackJPEG:
 
 	def check_JPEG(self):
 		try:
-			jpeg_header = unpack(">2B", self.data[0:2])
+			jpeg_header = self.fd.read(2)
 		except:
 			print("Error unpacking bytes")
 			sys.exit()
-		if jpeg_header == (255, 216):
+		if jpeg_header == b'\xff\xd8':
 			print("This is a JPEG image!")
 		else:
 			print("This is NOT a JPEG!")
 			sys.exit()
-
+				
 	def find_markers(self):
-		mark_array = []
-		index_array = []
+		self.mark_array = []
 		index = 2
 		
 		while True:
-			marker = unpack(">BBH", self.data[index:index+4])
-			if "{:02x}".format(marker[1]) == 'da':
-				mark_array.append(marker)
-				index_array.append(index)
+			marker = unpack(">BBH", self.fd.read(4))
+			marker = (index, ) + marker
+			self.mark_array.append(marker)
+			if "{:02x}".format(marker[2]) == 'da':
 				break
 			else:
-				mark_array.append(marker)
-				index_array.append(index)
-				index = index + marker[2] + 2
+				index = self.fd.tell() + marker[3] - 2
+				self.fd.seek(index)
 		
-		count = 0
-		while count < len(mark_array):
-			print("{:#06x}".format(index_array[count]), end=' ')
+		for m in self.mark_array:
+			print("{:#06x}".format(m[0]), end=' ')
 			print("Marker", end=' ')
-			print(hex(mark_array[count][0]) + "{:02x}".format(mark_array[count][1]), end=' ')
-			print("size=" + "{:#06x}".format(mark_array[count][2]))
-			count += 1
+			print(hex(m[1]) + "{:02x}".format(m[2]), end=' ')
+			print("size=" + "{:#06x}".format(m[3]))
 
-		# print(self.data[2:4].decode("utf-8", "ignore"))
-
-		for i in index_array:
-			# exif_check = bytes.decode(self.data[i+4:i+10])
-			exif_check = self.data[i+4:i+14].decode("utf-8", "ignore")
-			if (exif_check[0:4] == 'Exif'):
-				if (exif_check[6:8] != 'MM'):
+	def find_exif(self):
+		self.ifd_start = 0
+		for m in self.mark_array:
+			self.fd.seek(m[0]+4)
+			exif_check = self.fd.read(6)
+			if (exif_check[0:4] == b'Exif'):
+				self.endian_mark = self.fd.tell()
+				if (self.fd.read(2) != b'MM'):
+					print("This is a Little Endian Exif block.")
+					self.fd.close()
 					sys.exit()
 				else:
-					print("BIG ENDIAN")
-				
+					self.fd.read(2)
+					ifd_offset = unpack(">L", self.fd.read(4))
+					self.ifd_start = self.fd.tell() + ifd_offset[0] - 8
+					self.fd.seek(self.ifd_start)
+					break
 
+		self.ifd_entries = unpack(">H", self.fd.read(2))[0]
+		print("Number of IDF entries:", self.ifd_entries)
+
+	def find_ifds(self):
+		self.ifd_array = []
+		bytes_per_component = (0, 1, 1, 2, 4, 8, 1, 1, 2, 4, 8, 4, 8)
+
+		"""For loop puts ifd tags into array as tuples.
+		   Each ifd tuple consists of: 
+		   (tag, format, # of components, data/data offset, data length)"""
+		for x in range(0, self.ifd_entries):
+			ifd_info = unpack(">H", self.fd.read(2))
+			ifd_info = ifd_info + unpack(">H", self.fd.read(2))
+			ifd_info = ifd_info + unpack(">L", self.fd.read(4))
+			ifd_info = ifd_info + unpack(">L", self.fd.read(4))
+			ifd_info = ifd_info + (bytes_per_component[ifd_info[1]] * ifd_info[2],)
+			self.ifd_array.append(ifd_info)
+
+		for i in self.ifd_array:
+			tag_name = "{:#06x}".format(i[0])
+			print(tag_name, "{:<20}".format(self.tag_dict(tag_name)), ':', end=' ')
+			if (i[4] <= 4):
+				print(i[3])
+			else:
+				self.fd.seek(self.endian_mark + i[3])
+				ifd_data = self.fd.read(i[4])
+				print(bytes.decode(ifd_data))
+
+	def tag_dict(self, tag_hex):
+		if tag_hex == '0x010e':
+			return 'Image Description'
+		elif tag_hex == '0x010f':
+			return 'Make'
+		elif tag_hex == '0x0110':
+			return 'Model'
+		elif tag_hex == '0x0112':
+			return 'Orientation'
+		elif tag_hex == '0x011a':
+			return 'X Resolution'
+		elif tag_hex == '0x011b':
+			return 'Y Resolution'
+		elif tag_hex == '0x0128':
+			return 'Resolution Unit'
+		elif tag_hex == '0x0131':
+			return 'Software'
+		elif tag_hex == '0x0132':
+			return 'DateTime'
+		elif tag_hex == '0x013e':
+			return 'WhitePoint'
+		elif tag_hex == '0x013f':
+			return 'Primary Chromaticities'
+		elif tag_hex == '0x8298':
+			return 'Copyright'
+		else:
+			return 'Unavailable'
 
 
 	def exif(self):
 		self.open_file()
-		self.read_file()
+		# self.read_file()
 		self.check_JPEG()
 		self.find_markers()
+		self.find_exif()
+		self.find_ifds()
 
 
 def main():
